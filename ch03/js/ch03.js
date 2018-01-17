4ab0ffa62e7096f9c417aae1464e756a1cfb8be9
alexbaek <alexbaek31@gmail.com>
2018-01-17 10:28:34 +0900

    프로젝트 생성

---
+++ ch03/js/ch03.js
@@ -0,0 +1,13 @@
+var hotel = {
+    name : 'Park',
+    rooms : 120,
+    booked : 77
+};
+
+hotel.gym = true;
+hotel.pool = false;
+delete hotel.booked;
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
